f5e3407f6f14433e7c4fb97437f13f090656b6a7
JunjieBian <bain1998@outlook.com>
2019-09-19 17:34:32 +0800

    Update and rename src/main/java/com/adley/oauth/server/Oauth2ServerApp.java to src/main/java/com/adley/oauth/resource/Oauth2ResourceServerApp.java

---
+++ src/main/java/com/adley/oauth/resource/Oauth2ResourceServerApp.java
@@ -0,0 +1,7 @@
+package com.adley.oauth.resource;
+
+@Slf4j
+@SpringBootApplication
+public class Oauth2ResourceServerApp {
+    public static void main(String[] args) {StringApplication.run(Oauth2ResourceServerApp.class, args);}
+}
