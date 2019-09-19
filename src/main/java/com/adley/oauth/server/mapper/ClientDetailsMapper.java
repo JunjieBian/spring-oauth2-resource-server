dc81a3442d89054f04334ecde120580a06aebf98
JunjieBian <bain1998@outlook.com>
2019-09-19 17:45:39 +0800

    Delete ClientDetailsMapper.java

--- src/main/java/com/adley/oauth/server/mapper/ClientDetailsMapper.java
+++
@@ -1,7 +0,0 @@
-package com.adley.oauth.server.mapper;
-
-@Mapper
-public interface ClientDetailsMapper {
-  @Select(value="select client_id from oauth_client_details")
-  List<String> findAllIds();
-}
